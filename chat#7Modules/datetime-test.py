66bdd813b0081b62ffe0894a7c4047fffd165705
GF Blue <gfblue47@gmail.com>
2021-10-28 21:39:06 +0500

    math exersice

---
+++ chat#7Modules/datetime-test.py
@@ -0,0 +1,3 @@
+import datetime
+x = datetime.datetime(2017,6,1)
+print(x.strftime("Tody is %A %B %Y"))
---
+++ chat#7Modules/mathmodule.py
@@ -0,0 +1,12 @@
+import math
+x = math.sqrt(49)
+print(x)
+
+
+x = math.ceil(7.4)
+y = math.floor(1.4)
+print(x)
+print(y)
+
+x = math.pi
+print(x)
